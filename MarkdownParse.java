3043b435eb3bc56a61f73d73eec68c45981c8426
LeoJuJu05090822 <103226676+LeoJuJu05090822@users.noreply.github.com>
2022-04-24 19:53:18 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,7 +18,9 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-            
+            if(openBracket == -1 || closeBracket ==-1 ||openParen ==-1 || closeParen == -1 ){
+                break;
+            }
         }
 
         return toReturn;
